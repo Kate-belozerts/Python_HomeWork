7053ced5a5b2590392f63099e969f1c07a73253c
Kate Belozertseva <katebelozertseva@gmail.com>
2022-12-14 23:35:33 +0300

    Changes added

--- HomeWork05/task05.py
+++ HomeWork05/task05.py
@@ -35,3 +35,100 @@ def sequences(arr, first, second):
 
 
 sequences(numbers, start, next_element)
+
+## Новое решение:______________________________________________________________
+
+# numbers = [1, 5, 2, 3, 4, 6, 1, 7]
+# start = 0
+# next_element = 1
+ 
+ 
+# def sequences(arr, first, second):
+#     second_old = second
+#     yes = True
+#     size = len(arr)
+#     if second == size:
+#         yes = False
+#     res = []
+#     res.append(arr[first])
+#     while second < size:
+#         if res[-1] < arr[second]:
+#             res.append(arr[second])
+#             print(res)
+#             second += 1
+#         else:
+#             second += 1
+ 
+#     if yes:
+#         sequences(arr, first, second_old + 1)
+ 
+ 
+# for i in range(len(numbers)):
+#     sequences(numbers, i, next_element)
+
+
+
+# Гениальное решение Сергея через логичную рекурсию: ______________________________________________________________
+# from pprint import pprint
+ 
+ 
+# numbers = [1, 5, 2, 3, 4, 6, 1, 2, 7]
+# seq = set()
+ 
+ 
+# def find_sequences(arr, cur_i=0, cur_seq=tuple()):
+#     # Точка выхода: когда индекс вышел за границы списка
+#     if cur_i == len(arr):
+#         if len(cur_seq) > 1:
+#             seq.add(cur_seq)
+#         return
+ 
+#     # Ищем дальше, игнорируя текущий элемент
+#     find_sequences(arr, cur_i + 1, cur_seq)
+ 
+#     # Ищем дальше, используя текущий элемент, но только если он подходящий
+#     if (cur_seq and cur_seq[-1] <= arr[cur_i]) or not(cur_seq):
+#         find_sequences(arr, cur_i + 1, cur_seq + (arr[cur_i],))
+ 
+ 
+# find_sequences(numbers)
+ 
+# pprint(seq)
+# print(len(seq))
+
+
+
+
+
+# Решение Ильи без рекурсии: ______________________________________________________________
+
+# array = [1, 5, 2, 3, 4, 9, 6, 1, 6, 7, 8, 10, 10]
+
+# length = len(array)
+# spread_collection = {}
+# for i in range(length):
+#     spread_collection.update({i: [[array[i]]]})
+
+# print(spread_collection)
+
+# for i in range(length):
+#     tmp_item = spread_collection[i]
+#     tmp_small_item = []
+#     for small_item in tmp_item:
+#         for j in range(i + 1, length):
+#             if array[j] > small_item[-1]:
+#                 tmp_small_item = small_item.copy()
+#                 tmp_small_item.append(array[j])
+#                 spread_collection[j].append(tmp_small_item)
+
+# tmp_length = 0
+# for v in spread_collection:
+#     print(v, spread_collection[v])
+#     for item in spread_collection[v]:
+#         if len(item) > tmp_length:
+#             tmp_arr = [item]
+#             tmp_length = len(item)
+#         elif len(item) == tmp_length:
+#             tmp_arr.append(item)
+# print(tmp_length)
+# print(tmp_arr)
